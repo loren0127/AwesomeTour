39d5de45796c3964380568955377a2c2def361af
yunju <yunju@DESKTOP-Q0IQLJS>
2018-08-24 17:21:46 +0900

    새로 accregistercontroller 수정

--- awesometour/src/main/java/kr/spring/accom/controller/AccRegisterController.java
+++ awesometour/src/main/java/kr/spring/accom/controller/AccRegisterController.java
@@ -1,5 +1,11 @@
 package kr.spring.accom.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class AccRegisterController {
+	@RequestMapping("/accregister/accregisterMain.do")
+	public String form() {
+		return "accregisterMain";
+}
 
 }
