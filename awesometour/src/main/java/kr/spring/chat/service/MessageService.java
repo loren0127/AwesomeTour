efff39a6d05d1e7c98f643ea02b2da8c307be1db
ziaizon <ziaizon@gmail.com>
2018-09-12 18:58:03 +0900

    2018-09-12 06:57 MessageService

---
+++ awesometour/src/main/java/kr/spring/chat/service/MessageService.java
@@ -0,0 +1,13 @@
+package kr.spring.chat.service;
+
+import java.util.List;
+import java.util.Map;
+
+import kr.spring.chat.domain.MessageCommand;
+
+public interface MessageService {
+	public int selectMessageRowCount(Map<String, Object> map);
+	public List<MessageCommand> selectMessageList(Map<String, Object> map);
+	
+	public void insertMessageSend(MessageCommand command);
+}
