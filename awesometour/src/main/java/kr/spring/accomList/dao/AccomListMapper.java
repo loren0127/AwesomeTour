02c643ca0f373cc820aa7cef05ea616a13787be8
dmsdm <dmsdm@DESKTOP-D53G1TT>
2018-09-13 17:28:00 +0900

    listController와 ajaxController 수정

--- awesometour/src/main/java/kr/spring/accomList/dao/AccomListMapper.java
+++ awesometour/src/main/java/kr/spring/accomList/dao/AccomListMapper.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Select;
 
+import kr.spring.accom.domain.ImageCommand;
 import kr.spring.accomList.domain.AccomListCommand;
 
 
@@ -15,6 +16,8 @@ public interface AccomListMapper {
 	public String selectAcoomSerivce(int se_num);
 	public List<AccomListCommand> selectAccomTotallist(Map<String, Object> map);
 	public int selectAccomTotallistCount(Map<String,Object> map);
+	@Select("SELECT im_cover,im_cover_name,im_image2,im_image2_name FROM accom_image WHERE im_ac_num=#{im_ac_num}")
+	public ImageCommand selectImageView(Integer im_ac_num);
 	
 }
 
